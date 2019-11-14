a12711740ceeb624b83e02954db15adea8fb136e
aryaberi <54583051+aryaberi@users.noreply.github.com>
2019-11-14 08:22:26 +0700

    Update status.pl

--- status.pl
+++ status.pl
@@ -1,5 +1,5 @@
 :- dynamic(inventory/2).
-
+:- dynamic(healt/2).
 status :-
 	write('Your Tokemon : '), nl,
 	inventory(_,_)->(
@@ -20,3 +20,26 @@ status :-
 		write(' Inventory kosong'),nl
 	),
 !.
+  write('Your Enemy'),nl,
+  maxhealt(articuno,X),
+  asserta(healt(articuno,Y)),
+  Y is X
+  write('articuno'),nl,
+  write('healt:'),write(Y),nl,
+  write('type :water'),nl,
+  
+  maxhealt(moltres,A),
+  asserta(healt(moltres,B)),
+  B is A
+  write('moltres'),nl,
+  write('healt:'),write(B),nl,
+  write('type :fire'),nl,
+  
+  maxhealt(moltres,C),
+  asserta(healt(moltres,D)),
+  D is C
+  write('Zapdos'),nl,
+  write('healt:'),write(D),nl,
+  write('type :leave'),nl.
+  
+  
