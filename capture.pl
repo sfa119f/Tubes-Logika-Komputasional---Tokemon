27a68d499bec610c1cb55f62d8f9d791f18e0b92
sfa119f <syarifuddinfa@gmail.com>
2019-11-16 01:26:24 +0700

    pembenahan status dan tambah capture, drop, pick

---
+++ capture.pl
@@ -0,0 +1,34 @@
+capture:-
+	play(false),
+	write('Game belum dimulai'), nl,
+	nl,!.
+capture:-
+	player([A,B]),
+	musuh(_,[X,Y]),
+	A\==X, B\==Y,
+	write('Tidak ada musuh yang dicapture.'),
+	nl,!.
+capture:-
+	player([A,B]),
+	musuh(Obj,[A,B]),
+	healthM(Obj,X),
+	X\==0,
+	write('Musuh belum menyerah, lawan dulu dong!'),
+	nl,!.
+capture:-
+	jumInv(X),X=:=6,
+	write('Inventory penuh, drop inventory duls.'),
+	nl,!.
+capture:-
+	jumInv(X),
+	Xn is X+1,
+	asserta(jumInv(Xn)),
+	player([A,B]),
+	musuh(Obj,[A,B]),
+	asserta(inventory(Obj)),
+	maxhealth(Obj,A),
+	asserta(healthP(Obj,A)),
+	nl,!.
+
+
+
--- status.pl
+++ status.pl
@@ -1,45 +1,32 @@
-:- dynamic(inventory/2).
+:- dynamic(inventory/1).
+:- dynamic(legend/1).
 :- dynamic(healt/2).
+:- dynamic(type/2).
 status :-
-	write('Your Tokemon : '), nl,
-	inventory(_,_)->(
-		forall(inventory(Object,Atribut),
+	write('YOUR TOKEMON :'), nl,
+	forall(
+	    inventory(Obj),
 		(
 			write(Obj), nl,
-			write('Type : '),write(Atribut),
-			(
-				(type(Obj,_));
-			),
-			write('Health : '),write(Atribut),
-			(
-				(health(Obj,_));
-			)
-			,nl
-		))
-	);(
-		write(' Inventory kosong'),nl
+			healthP(Obj,X),write('Health : '),write(X),nl,
+			type(Obj,X),write('Type   : '),nl,nl
+		)
 	),
-!.
-  write('Your Enemy'),nl,
-  maxhealt(articuno,X),
-  asserta(healt(articuno,Y)),
-  Y is X
-  write('articuno'),nl,
-  write('healt:'),write(Y),nl,
-  write('type :water'),nl,
-  
-  maxhealt(moltres,A),
-  asserta(healt(moltres,B)),
-  B is A
-  write('moltres'),nl,
-  write('healt:'),write(B),nl,
-  write('type :fire'),nl,
-  
-  maxhealt(moltres,C),
-  asserta(healt(moltres,D)),
-  D is C
-  write('Zapdos'),nl,
-  write('healt:'),write(D),nl,
-  write('type :leave'),nl.
-  
-  
+	nl,
+	write('YOUR ENEMY :'),nl,
+	forall(
+	    legend(Msh),
+		(
+			write(Msh), nl,
+			healthM(Msh,X),write('Health : '),write(X),nl,
+			type(Msh,X),write('Type   : '),nl,nl
+		)
+	),
+	nl,!.
+
+
+
+
+
+
+
