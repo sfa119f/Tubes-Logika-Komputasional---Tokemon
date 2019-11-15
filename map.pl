8c720b531e169f153c3ab128294af4a6ac6f421a
Rahmannnnn <arifingatrahman@gmail.com>
2019-11-15 19:15:46 +0700

    map sama move jadi satu

--- map.pl
+++ map.pl
@@ -1,46 +1,110 @@
-:- include('main.pl').
+:- dynamic(panjangPeta/1).
 :- dynamic(lebarPeta/1).
-:- dynamic(tinggiPeta/1).
-:- dynamic(gymPeta/2).
+:- dynamic(gym/2).
+:- dynamic(player/2).
 
-map :-
-	random(10,20,X),
-	random(10,20,Y),
-	asserta(lebarPeta(X)), asserta(tinggiPeta(Y)),!.
-	
-	random(0,X,A),
-	random(0,Y,B),
-	asserta(gymPeta(A,B),!.
 
-isBorderAtas(_,Y) :-
-    Y=:=0
-    ,!.
-isBorderKiri(X,_) :-
-    X=:=0,
-    !.
-isBorderBawah(_,Y) :-
-    tinggiPeta(T),
-    YMax is T+1,
-    Y=:=YMax,
-    !.
-isBorderKanan(X,_) :-
-    lebarPeta(L),
-    XMax is L+1,
-    X=:=XMax,
+makeMap(Panjang,Lebar) :-
+	random(10,20,Panjang),
+	random(10,20,Lebar),
+	asserta(panjangPeta(Panjang)),
+	asserta(lebarPeta(Lebar)),
+        !,
+	random(10,Panjang,X),
+	random(10,Lebar,Y),
+	asserta(gym(X,Y)),
+	!,
+	random(10,Panjang,A),
+	random(10,Lebar,B),
+	asserta(player(A,B)),
 	!.
 
+map :- printMap(0,0).
+
+isBorderAtas(_,Y) :-
+    Y =:= 0
+    ,!.
+isBorderKiri(X,_) :-
+    X =:= 0,
+    !.
+isBorderBawah(_,Y) :-
+    lebarPeta(T),
+    Y =:= T+1,
+    !.
+isBorderKanan(X,_) :-
+    panjangPeta(L),
+    X =:= L+1,
+	!.
+%Print Lokasi Player
 printMap(X,Y) :-
-    isBorderKanan(X,Y), !, write('X').
+	player(A,B),
+	X =:= A,
+	Y =:= B,
+	write(' P '),
+	printMap(X+1,Y).
+
+%Print Lokasi Gym
 printMap(X,Y) :-
-    isBorderKiri(X,Y), !, write('X').
+	gym(A,B),
+	X =:= A,
+	Y =:= B,
+	write(' G '),
+	printMap(X+1,Y).
 printMap(X,Y) :-
-    isBorderAtas(X,Y), !, write('X').
+	isBorderBawah(X,Y), isBorderKanan(X,Y), !, write(' X ').
 printMap(X,Y) :-
-	isBorderBawah(X,Y), !, write('X').
-printMap(_,_) :-
-	write('-').
-	
+	isBorderKiri(X,Y), !, write(' X '), printMap(X+1,Y).
+printMap(X,Y) :-
+	isBorderKanan(X,Y), !, write(' X '), nl, printMap(0,Y+1).
+printMap(X,Y) :-
+	isBorderAtas(X,Y), !, write(' X '), printMap(X+1,Y).
+printMap(X,Y) :-
+	isBorderBawah(X,Y), !, write(' X '), printMap(X+1,Y).
+printMap(X,Y) :-
+	write(' - '), printMap(X+1,Y), !.
 
 
-	
+% MOVE
+% 1. North (Atas)
+n :- player(X,Y), Ya is Y-1, isBorderAtas(X,Ya), write('Pagar bro'), !.
+n :- player(X,Y), Ya is Y-1, gym(A,B), X \== A, Ya \== B, retractall(player(_,_)), asserta(player(X, Ya)), write('Anda bergerak ke Utara, anda berada pada tanah kosong.'), !.
+n :- player(X,Y), Ya is Y-1, gym(A,_), X \== A, retractall(player(_,_)), asserta(player(X, Ya)), write('Anda bergerak ke Utara, anda berada pada tanah kosong.'), !.
+n :- player(X,Y), Ya is Y-1, gym(_,B), Ya \== B, retractall(player(_,_)), asserta(player(X, Ya)), write('Anda bergerak ke Utara, anda berada pada tanah kosong.'), !.
+n :- player(X,Y), Ya is Y-1, gym(A,B), X =:= A, Ya =:= B, retractall(player(_,_)), asserta(player(X, Ya)), write('Anda bergerak ke Utara, anda berada pada Gym.'), !.
+
+% 2. South (Bawah)
+s :- player(X,Y), Ya is Y+1, isBorderBawah(X,Ya), write('Pagar bro'), !.
+s :- player(X,Y), Ya is Y+1, gym(A,B), X \== A, Ya \== B, retractall(player(_,_)), asserta(player(X, Ya)), write('Anda bergerak ke Selatan, anda berada pada tanah kosong.'), !.
+s :- player(X,Y), Ya is Y+1, gym(A,_), X \== A, retractall(player(_,_)), asserta(player(X, Ya)), write('Anda bergerak ke Selatan, anda berada pada tanah kosong.'), !.
+s :- player(X,Y), Ya is Y+1, gym(_,B), Ya \== B, retractall(player(_,_)), asserta(player(X, Ya)), write('Anda bergerak ke Selatan, anda berada pada tanah kosong.'), !.
+s :- player(X,Y), Ya is Y+1, gym(A,B), X =:= A, Ya =:= B, retractall(player(_,_)), asserta(player(X, Ya)), write('Anda bergerak ke Selatan, anda berada pada Gym.'), !.
+
+% 3. East (Kanan)
+e :- player(X,Y), Xa is X+1, isBorderKanan(Xa,Y), write('Pagar bro'), !.
+e :- player(X,Y), Xa is X+1, gym(A,B), Xa \== A, Y \== B, retractall(player(_,_)), asserta(player(Xa, Y)), write('Anda bergerak ke Timur, anda berada pada tanah kosong.'), !.
+e :- player(X,Y), Xa is X+1, gym(A,_), Xa \== A, retractall(player(_,_)), asserta(player(Xa, Y)), write('Anda bergerak ke Timur, anda berada pada tanah kosong.'), !.
+e :- player(X,Y), Xa is X+1, gym(_,B), Y \== B, retractall(player(_,_)), asserta(player(Xa, Y)), write('Anda bergerak ke Timur, anda berada pada tanah kosong.'), !.
+e :- player(X,Y), Xa is X+1, gym(A,B), Xa =:= A, Y =:= B, retractall(player(_,_)), asserta(player(Xa, Y)), write('Anda bergerak ke Timur, anda berada pada Gym.'), !.
+
+% 4. West (Kiri)
+w :- player(X,Y), Xa is X-1, isBorderKiri(Xa,Y), write('Pagar bro'), !.
+w :- player(X,Y), Xa is X-1, gym(A,B), Xa \== A, Y \== B, retractall(player(_,_)), asserta(player(Xa, Y)), write('Anda bergerak ke Barat, anda berada pada tanah kosong.'), !.
+w :- player(X,Y), Xa is X-1, gym(A,_), Xa \== A, retractall(player(_,_)), asserta(player(Xa, Y)), write('Anda bergerak ke Barat, anda berada pada tanah kosong.'), !.
+w :- player(X,Y), Xa is X-1, gym(_,B), Y \== B, retractall(player(_,_)), asserta(player(Xa, Y)), write('Anda bergerak ke Barat, anda berada pada tanah kosong.'), !.
+w :- player(X,Y), Xa is X-1, gym(A,B), Xa =:= A, Y =:= B, retractall(player(_,_)), asserta(player(Xa, Y)), write('Anda bergerak ke Barat, anda berada pada tanah kosong.'), !.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
