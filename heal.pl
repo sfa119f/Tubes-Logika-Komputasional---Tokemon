d0a5075c07a52bbd14202b81edf69b9e0952c556
aryaberi <54583051+aryaberi@users.noreply.github.com>
2019-11-16 07:49:29 +0700

    Update heal.pl

--- heal.pl
+++ heal.pl
@@ -1,17 +1,28 @@
-heal(X) :- Player(X,Y),
-		   GYM(A,B),
-		   X =\= A, Y =\= B,
-		   write('kamu sedang tidak di gym'),nl,!.
-heal(X) :- healt(X,Y), 
-		   Y =:= 0,
-		   write('pokemon kamu sudah mati'),nl,!.
-heal(X) :- pernah(d),
-		   d =:= 1
-		   write('Pokemon kamu sudah pernah di heal'),nl,!.
-heal(X) :- retract(healt(X,Y)),
-		   maxhealt(X,Z), 
-		   newY is Z, 
-		   asserta(X,newY),
-		   pernah(d),
-		   d is 1.
+:- dynamic(healt/2).
+:- dynamic(pernah/2).
+
+heal(_) :- play(false),
+	   write ('gamenya belum mulai pencet start dulu'),!.
+
+heal(obj) :- player([X,Y]),gym([A,B]),
+	   X =\= A, Y =\= B,
+	   write('kamu sedang tidak di gym'),nl,!.
+heal(obj):-
+	   \+inventory(Obj),!,write('Kamu tidak punya '),write(Obj),write(' di inventorymu'),
+	    nl,!.
+
+heal(obj) :- healt(obj,Y), 
+           Y =:= 0,
+	   write('pokemon kamu sudah mati'),nl,!.
+
+heal(obj) :- pernah(obj,d),
+	   d =:= 1
+           write('Pokemon kamu sudah pernah di heal'),nl,!.
+
+heal(obj) :- retract(healt(obj,Y)),
+	    maxhealt(obj,Z), 
+            newY is Z, 
+	    asserta(obj,newY),
+	    pernah(obj,d),
+	    d is 1.
 		    
