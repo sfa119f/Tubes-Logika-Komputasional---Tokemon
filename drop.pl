5d374fb5dbfb338e0c0cc63de1e42083ca8a4ed4
sfa119f <syarifuddinfa@gmail.com>
2019-11-18 00:22:59 +0700

    update dropP dropM

--- drop.pl
+++ drop.pl
@@ -14,3 +14,30 @@ drop(Obj):-
 	asserta(jumInv(Xn)),
 	write('Sayang sekali, Tokemon '),write(Obj),write(' telah dilepaskan'),
 	nl,!.
+
+dropP(Obj):-
+	retract(inventory(Obj)),
+	retract(healthP(Obj,_)),
+	jumInv(X),
+	Xn is X-1,
+	retract(jumInv(_)),
+	asserta(jumInv(Xn)),
+	!.
+
+dropM(Obj):-
+	retract(musuh(Obj,_)),
+	retract(healthM(Obj,_)),
+	jumMusuh(X),
+	Xn is X-1,
+	retract(jumMusuh(_)),
+	asserta(jumMusuh(Xn)),
+	(jenis(Obj,legendary)->
+	    retract(legend(Obj)),
+	    jumLegend(Y),
+	    Yn is Y-1,
+	    retract(jumLegend(_)),
+	    asserta(jumLegend(Yn))
+	    ;
+	    !
+	),
+	!.
